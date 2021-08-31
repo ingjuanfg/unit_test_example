6c28b105d8092fbf42c61a135e351824ff25d89d
ingjuanfg <juan.isfg@gmail.com>
2021-08-31 17:31:23 -0500

    Primera prueba unitaria con 2 estados

--- src/test/java/com/ingjuanfg/PersonTest.java
+++ src/test/java/com/ingjuanfg/PersonTest.java
@@ -1,5 +1,6 @@
 package com.ingjuanfg;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,8 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PersonTest {
 
     @Test
-    public void suma(){
+    public void sumaEntreDosNumerosExitosaTest(){
         Calculadora calculadora = new Calculadora();
-        assertEquals(8, calculadora.suma(5,1));
+        assertEquals(6, calculadora.suma(5,1));
+    }
+
+    @Test
+    public void sumaEntreDosNumerosFallidaTest(){
+        Calculadora calculadora = new Calculadora();
+        Assertions.assertNotEquals(8, calculadora.suma(5,1));
     }
 }
